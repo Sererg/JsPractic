24ca5f02038c0e80c67db012fa2df69b6c5be324
Sergey <svovyk@mail.ru>
2023-09-27 11:43:27 +0300

    Работа с массивами

--- Tasks/Task4.js
+++ Tasks/Task4.js
@@ -20,3 +20,41 @@ function standardizeStrings(arr){
    })
 }
 standardizeStrings(favoriteCities);
+
+
+
+
+
+
+
+const someString = 'This is some strange string';
+
+function strReverse(str){
+    if(typeof str !== "string"){
+        console.log('Error');
+    }else{
+        str.split("").reverse().join("");
+        console.log(str)
+    }
+}
+strReverse(someString);
+
+
+
+
+
+
+
+const baseCurrencies = ['USD', 'EUR',];
+const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
+
+function availableCurr(arr, missingCurr){
+    if (arr.length == 0){
+       console.log( 'Нет доступных валют');
+    }else{
+        let res = 'Доступные валюты:\n';
+        arr.filter(item => item !== missingCurr).forEach((curr) => res += `${curr}\n`);        
+        console.log(res)
+    }
+}
+availableCurr([...baseCurrencies, ...additionalCurrencies], 'CNY')
