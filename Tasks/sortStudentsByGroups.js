eb56bcdfb22f95af1f73f7be2743221db35246a9
Sergey <svovyk@mail.ru>
2023-09-28 18:59:23 +0300

    Соптировка студентов

---
+++ Tasks/sortStudentsByGroups.js
@@ -0,0 +1,24 @@
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Bris', 'Sandra', 'Cris', 'Tris', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    // console.log(arr.sort())
+
+    for (let i = 0; i < students.length+1; i++ ){
+        let studSort = (arr.sort().slice(i, i + 3));
+        i += 2
+       
+        if (studSort[0,1,2] != undefined) {
+             console.log(studSort)
+            //  console.log("1") 
+        } else if (studSort == false) {
+            // console.log("3")
+            console.log(`Оставшиеся студенты: -`)
+        }else if (studSort[2] == undefined || studSort[1] == undefined || studSort.inclides() != true) {
+            // console.log("2")
+            console.log(`Оставшиеся студенты:${studSort}`)
+        } 
+
+
+    }
+}
+sortStudentsByGroups(students);
