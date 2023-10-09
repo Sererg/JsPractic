a622e752b9be9d3c52e83dd01f6535315d21e755
Sergey <svovyk@mail.ru>
2023-10-09 21:02:13 +0300

    +

---
+++ Tasks/tabs/js/script.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded', () =>{
+
+    const tabs = document.querySelectorAll('.tabheader__item'),
+          tabsContent = document.querySelectorAll('.tabcontent'),
+          tabsParent = document.querySelector('.tabheader__items');
+
+    function hideTabcontent() {
+        tabsContent.forEach(item => {
+            // item.classList.add('hide');
+            // item.classList.remove('show','import');
+            item.style.display = 'none';
+        });
+
+        tabs.forEach(item => {
+            item.classList.remove('tabheader__item_active');
+        });
+    }
+
+    function showTabContent(i = 0) {
+        // tabsContent[i].classList.add('show', 'import');
+        // tabsContent[i].classList.remove('hide');
+        tabsContent[i].style.display = 'block';
+        tabs[i].classList.add('tabheader__item_active');
+    }
+
+    hideTabcontent();
+    showTabContent();
+
+    tabsParent.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if(target && target.classList.contains('tabheader__item')) {
+            tabs.forEach((item, i ) =>{
+                if(target == item){
+                    hideTabcontent();
+                    showTabContent(i);
+                }
+            })
+        }
+    });
+});
