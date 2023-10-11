18c76bf1cc47d38cbeacd4b5e0fef5035445bf0f
Sergey <svovyk@mail.ru>
2023-10-11 15:04:32 +0300

    контекст вызова

--- Tasks/this.js
+++ Tasks/this.js
@@ -1,56 +1,56 @@
 //1
-function showThis(a, b) {
-    console.log(this);
-    function sum() {
-        console.log(this);
-        return a + b
-    }
+// function showThis(a, b) {
+//     console.log(this);
+//     function sum() {
+//         console.log(this);
+//         return a + b
+//     }
 
-console.log(sum());
+// console.log(sum());
 
-}
-showThis(4, 5);
-.//2
-const obj = {
-    a: 20,
-    b: 15,
-    sum: function() {
-        console.log(this);
-    }
-};
-obj.sum();
+// }
+// showThis(4, 5);
+//2
+// const obj = {
+//     a: 20,
+//     b: 15,
+//     sum: function() {
+//         console.log(this);
+//     }
+// };
+// obj.sum();
 //3
-function user(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function () {
-        console.log(`hello ${this.name}`);
-    };
-}
+// function user(name, id) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.hello = function () {
+//         console.log(`hello ${this.name}`);
+//     };
+// }
 
-let ivan = new user('Ivan', 23);
+// let ivan = new user('Ivan', 23);
 
 //4
-function sayName(surname) {
-    console.log(this);
-    console.log(this.name + surname);
-}
+// function sayName(surname) {
+//     console.log(this);
+//     console.log(this.name + surname);
+// }
 
-const user = { 
-    name:"john"
-};
+// const user = { 
+//     name:"john"
+// };
 
-sayName.call(user, 'Smith');
-sayName.apply(user, ['Smith']);
+// sayName.call(user, 'Smith');
+// sayName.apply(user, ['Smith']);
 
-function count (num) {
-    return this*num;
-}
+// function count (num) {
+//     return this*num;
+// }
 
-const double = count.bind(2);
-console.log(double(3));
-console.log(double(12));
+// const double = count.bind(2);
+// console.log(double(3));
+// console.log(double(12));
 
 //5
 
