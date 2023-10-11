13781279592213fd188d0729b79d033dbddf1755
Sergey <svovyk@mail.ru>
2023-10-11 13:10:35 +0300

    создаю модальное окно

--- Tasks/tabs/js/script.js
+++ Tasks/tabs/js/script.js
@@ -103,4 +103,37 @@ document.addEventListener('DOMContentLoaded', () =>{
     }
 
     setClock('.timer', deadline);
+
+    // Modal
+
+    const modalTrigger = document.querySelectorAll('[data-modal]'),
+          modal = document.querySelector('.modal'),
+          modalCloseBtn = document.querySelector('[data-close]');
+    
+    modalTrigger.forEach(btn => {
+        btn.addEventListener('click', () => {
+            modal.classList.add('show');
+            modal.classList.remove('hide');
+            document.body.style.overflow = 'hidden';
+        });
+    });
+
+    function closeModel(){
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow = '';
+    }
+    modalCloseBtn.addEventListener('click', closeModel);
+
+    modal.addEventListener('click', (e)=> {
+        if (e.target === modal){
+            closeModel();
+        }
+    });
+
+    document.addEventListener('keydown', (e) => {
+        if(e.code === 'Escape' && modal.classList.contains('show')) {
+            closeModel();
+        }
+    })
 });
