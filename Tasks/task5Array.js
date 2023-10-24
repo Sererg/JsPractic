2c135940e2548bd09ce9c3f60ca444c6400a7a95
Sergey <svovyk@mail.ru>
2023-10-24 13:14:58 +0300

    Задания на работу с методами массивов

--- Tasks/task5Array.js
+++ Tasks/task5Array.js
@@ -60,4 +60,44 @@ function checkFilms(arr) {
 }
 console.log(checkFilms(tranformedArray))
 
+//Задачи:
 
+// 1) У вас есть небольшой массив с данными о доходах каждой торговой точки. Напишите функцию getPositiveIncomeAmount, которая принимает этот массив данных и возвращает сумму только положительных значений из каждого объекта. (число)
+
+// Пример:
+
+// getPositiveIncomeAmount(funds) => 13300
+
+// 2) Напишите функцию getTotalIncomeAmount, которая тоже принимает этот массив данных. Если хотя бы один из объектов содержит отрицательное значение поля amount, то функция возвращает сумму всех значений. (число) Если таких значений нет - запускается функция getPositiveIncomeAmount с тем же массивом данных.
+
+// Пример:
+
+// getTotalIncomeAmount(funds) => -500
+
+const funds = [
+    {amount: -1400},
+    {amount: 2400},
+    {amount: -1000},
+    {amount: 500},
+    {amount: 10400},
+    {amount: -11400}
+];
+
+function getPositiveIncomeAmount (arr) {
+    return arr.filter(item => {
+        return item.amount > 0}).map(item => item.amount).reduce((summ, current) => {
+            return summ + current;
+        });
+};
+
+
+function getTotalIncomeAmount (arr) {
+    if(arr.some(item => item.amount < 0)) {
+       return arr.map(item => item.amount).reduce((summ, current) => {
+            return summ + current;
+        });
+    } else {
+        return getPositiveIncomeAmount(arr);
+    }
+}
+console.log(getTotalIncomeAmount(funds));
