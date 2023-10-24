9cc02f36759fc430acbec69a4868608e4f11362b
Sergey <svovyk@mail.ru>
2023-10-24 10:37:22 +0300

    Fetch API

--- Tasks/tabs/js/script.js
+++ Tasks/tabs/js/script.js
@@ -255,31 +255,31 @@ document.addEventListener('DOMContentLoaded', () =>{
                 `;
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'js/server.php');
-
-           request.setRequestHeader('Content-type', 'application/json');
             const formData = new FormData(form);
 
             const object = {};
             formData.forEach(function(value, key){
                 object[key] = value;
             });
-            
-            const json = JSON.stringify(object);
 
-            request.send(json);
-
-            request.addEventListener('load', () => {
-                if(request.status === 200) {
-                    console.log(request.response);
-                    showThanksModal(message.succes);
-                    form.reset();
-                    statusMessage.remove();
-                } else { 
-                    showThanksModal(message.failure);
-                }
-            });
+            fetch('js/server.php', {
+                method:"POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then(data => {
+                console.log(data);
+                showThanksModal(message.succes);
+                form.reset();
+                statusMessage.remove();
+            }).catch(() => {
+                showThanksModal(message.failure);
+            }).finally(() => {
+                form.reset();
+            })
         });
     }
 
