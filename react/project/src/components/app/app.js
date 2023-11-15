047006c198e7796e2f8a29be58960f705eeaa9b5
Sergey <svovyk@mail.ru>
2023-11-15 11:53:41 +0300

    добавил подсчет сотрудников

--- react/project/src/components/app/app.js
+++ react/project/src/components/app/app.js
@@ -13,9 +13,9 @@ class App extends Component {
     super(props);
     this.state = {
       data: [
-        {name:"John C." , salary: 800, increase: false, id:1 },
-        {name:"Alex S." , salary: 3000, increase: true, id:2 },
-        {name:"Carl W." , salary: 15000, increase: false, id:3 },
+        {name:"John C." , salary: 800, increase: false, rise: false, id:1 },
+        {name:"Alex S." , salary: 3000, increase: false, rise: false, id:2 },
+        {name:"Carl W." , salary: 15000, increase: false, rise: false, id:3 },
       ]
     }
 
@@ -36,6 +36,7 @@ class App extends Component {
       name,
       salary,
       increase:false,
+      rise: false,
       id: this.maxId++
     }
     this.setState(({data}) => {
@@ -46,11 +47,28 @@ class App extends Component {
     })
   }
 
+  onToggleProp = (id,prop) => {
+    this.setState(({data}) => ({
+      data:data.map(item => {
+        if(item.id === id) {
+            return{...item, [prop]: !item[prop] }
+        }
+        return item;
+      })
+    }))
+  }
+
+
+
 
   render() {
+    const employees = this.state.data.length;
+    const increased = this.state.data.filter(item => item.increase).length;
     return (
       <div className="app">
-          <AppInfo />
+          <AppInfo 
+          employees={employees}
+          increased={increased}/>
   
           <div className="search-panel">
               <SearchPanel/>
@@ -59,11 +77,11 @@ class App extends Component {
         
           <EmployeesList 
           data={this.state.data}
-          onDelete={this.deleteItem}/>
+          onDelete={this.deleteItem}
+          onToggleProp={this.onToggleProp}/>
           <EmployeesAddForm
           onAdd={this.addItem}/>
-  
-  
+
       </div>
     );
   }
--- react/project/src/components/employees-list/employees-list.js
+++ react/project/src/components/employees-list/employees-list.js
@@ -2,14 +2,15 @@ import EmployeesListItem from "../employees-list-item/employees-list-item";
 
 import './employees-list.css';
 
-const EmployeesList = ({data, onDelete}) => {
+const EmployeesList = ({data, onDelete, onToggleProp}) => {
 
     const elements = data.map(item => {
         const {id,...itemProps} = item;
         return (
             <EmployeesListItem
-             key={id} name = {item.name} salary={item.salary} increase={item.increase} //{...item} спрет оператор сделает точно также 
-             onDelete={() => onDelete(id)}/> 
+             key={id} name = {item.name} salary={item.salary} increase={item.increase} rise={item.rise} //{...item} спрет оператор сделает точно также 
+             onDelete={() => onDelete(id)}
+             onToggleProp={(e) => onToggleProp(id, e.currentTarget.getAttribute('data-toggle'))}/> 
         )
     })
     return (
