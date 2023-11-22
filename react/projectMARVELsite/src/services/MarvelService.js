fb6e9943516f657ecd8231e2738190fbc4c3e4b2
Sergey <svovyk@mail.ru>
2023-11-22 11:47:37 +0300

    Трансформация данных,компонент случайный персонаж

--- react/projectMARVELsite/src/services/MarvelService.js
+++ react/projectMARVELsite/src/services/MarvelService.js
@@ -14,12 +14,24 @@ class MarvelService {
         return await res.json();
     }
 
-    getAllChatacters = () => {
-        return this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+    getAllChatacters = async () => {
+        const res = this.getResource(`${this._apiBase}characters?limit=9&offset=210&${this._apiKey}`);
+        return res.data.results.map(this._transformCharacter);
     }
 
-    getChatacter = (id) => {
-        return this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+    getChatacter = async (id) => {
+        const res = await this.getResource(`${this._apiBase}characters/${id}?${this._apiKey}`);
+        return this._transformCharacter(res.data.results[0]);
+    }
+
+    _transformCharacter = (char) => {
+        return{
+            name: char.name,
+            description: char.description ? `${char.description.slice(0, 210)}...` : 'There is no description for this character',
+            thumbnail: char.thumbnail.path + '.' + char.thumbnail.extension,
+            homepage: char.urls[0].url,
+            wike: char.urls[1].url
+        }
     }
 }
 
