0f015536b327e5f7d90009b95f2cab1a4ff2f99b
Sergey <svovyk@mail.ru>
2023-12-01 14:01:36 +0300

    +

--- react/projectMARVELsite/src/components/charList/CharList.js
+++ react/projectMARVELsite/src/components/charList/CharList.js
@@ -12,7 +12,7 @@ class CharList extends Component {
         loading: true,
         error: false,
         newItemLoading: false,
-        offset: 1541,
+        offset: 240,
         charEnded: false
     }
     
