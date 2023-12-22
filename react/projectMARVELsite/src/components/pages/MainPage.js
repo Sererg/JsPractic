2fe45960bcdfd1b3e6e8b5bb2b90bfdb18c59bf8
Sergey <svovyk@mail.ru>
2023-12-22 10:30:44 +0300

    добваил поиск персонажей

--- react/projectMARVELsite/src/components/pages/MainPage.js
+++ react/projectMARVELsite/src/components/pages/MainPage.js
@@ -4,8 +4,10 @@ import RandomChar from "../randomChar/RandomChar";
 import CharList from "../charList/CharList";
 import CharInfo from "../charInfo/CharInfo";
 import ErrorBoundary from "../errorBoundary/ErrorBoundary";
+import CharSearchForm from "../charSearchform/charSearchForm";
 
 import decoration from '../../resources/img/vision.png';
+
 
 const MainPage = () => {
     
@@ -25,9 +27,15 @@ const MainPage = () => {
                 <ErrorBoundary>
                     <CharList onCharSelected = {onCharSelected}/>
                 </ErrorBoundary>
-                <ErrorBoundary>
-                    <CharInfo charId={selectedChar}/>
-                </ErrorBoundary>
+                <div>
+                    <ErrorBoundary>
+                        <CharInfo charId={selectedChar}/>
+                    </ErrorBoundary>
+                    <ErrorBoundary>
+                        <CharSearchForm charId={selectedChar}/>
+                    </ErrorBoundary>
+                </div>
+                
             </div>
             <img className="bg-decoration" src={decoration} alt="vision"/>
         </>
