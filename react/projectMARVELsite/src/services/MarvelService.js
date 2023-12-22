6c2ce744df065bd4a12cebd7ca25e795817923d0
Sergey <svovyk@mail.ru>
2023-12-22 10:33:02 +0300

    Поиск персонажа

--- react/projectMARVELsite/src/services/MarvelService.js
+++ react/projectMARVELsite/src/services/MarvelService.js
@@ -15,6 +15,11 @@ const useMarvelService = () => {
         return res.data.results.map(_transformCharacter);
     }
 
+    const getCharacterByName = async (name) => {
+		const res = await request(`${_apiBase}characters?name=${name}&${_apiKey}`);
+		return res.data.results.map(_transformCharacter);
+	};
+
     const getCharacter = async (id) => {
         const res = await request(`${_apiBase}characters/${id}?${_apiKey}`);
         return _transformCharacter(res.data.results[0]);
@@ -61,7 +66,7 @@ const useMarvelService = () => {
 	};
 
 
-    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic}
+    return {loading, error, getAllCharacters, getCharacter, clearError, getAllComics, getComic, getCharacterByName}
 }
 
 
