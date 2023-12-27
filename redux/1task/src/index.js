74173f59742e01a415c16b06ddd9159bc71edd83
Sergey <svovyk@mail.ru>
2023-12-27 10:44:06 +0300

    создание приложения

---
+++ redux/1task/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+console.log('Hello Redux!');
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <>
+    
+    </>
+  </React.StrictMode>
+);
+
+
