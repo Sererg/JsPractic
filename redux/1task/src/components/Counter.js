98b22b489448ea1ba9e9193db5e0e2135508b699
Sergey <svovyk@mail.ru>
2023-12-28 11:25:36 +0300

    connect

--- redux/1task/src/components/Counter.js
+++ redux/1task/src/components/Counter.js
@@ -1,13 +1,45 @@
+import { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from '../actions';
 
-const Counter = ({counter, inc, dec, rnd}) => {
-    return (
-        <div  className="jumbotron">
-        <h1>{counter}</h1>
-        <button onClick={dec} className="btn btn-primary">DEC</button>
-        <button onClick={inc} className="btn btn-primary">INC</button>
-        <button onClick={rnd} className="btn btn-primary">RND</button>
-    </div>
-    )
+
+// const Counter = ({counter, inc, dec, rnd}) => {
+//     return (
+//         <div  className="jumbotron">
+//         <h1>{counter}</h1>
+//         <button onClick={dec} className="btn btn-primary">DEC</button>
+//         <button onClick={inc} className="btn btn-primary">INC</button>
+//         <button onClick={rnd} className="btn btn-primary">RND</button>
+//     </div>
+//     )
+// }
+
+class Counter  extends Component {
+    render() {
+
+        const {counter, inc, dec, rnd} = this.props
+        return (
+            <div  className="jumbotron">
+            <h1>{counter}</h1>
+            <button onClick={dec} className="btn btn-primary">DEC</button>
+            <button onClick={inc} className="btn btn-primary">INC</button>
+            <button onClick={rnd} className="btn btn-primary">RND</button>
+        </div>
+        )
+    }
 }
 
-export default Counter;
+
+
+const mapStateToProps = (state) => {
+    return {
+        counter: state.value
+    }
+}
+
+// const mapDispatchToProps = (dispatch) => {
+//      return bindActionCreators(actions, dispatch)
+// }    
+
+
+export default connect(mapStateToProps, actions)(Counter);
